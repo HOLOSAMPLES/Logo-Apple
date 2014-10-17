0d8f082c34ad918f280d28f719daa9e5da0136f7
HOLOSAMPLES <hanlong@notmatter.com>
2014-10-17 10:23:05 +0800

    holoide commit

---
+++ js/default.js
@@ -0,0 +1,91 @@
+//FileStart:default.js 
+var mesh1, mesh2;
+ var newMeshReady = false;
+ var sizeMesh1 = 22;
+ var sizeMesh2 = 8;
+ var radius1 = 3;
+ var radius2x = 12;
+ var radius2z = 10;
+ var omega1 =  1.032;
+ var omega2 = -3.729;
+ var time_s = 0;
+ var time1 = 0;
+ var time0 = 0;
+ var windowWidth = window.innerWidth, windowHeight = window.innerHeight;
+ //LEIA_initAPI(true);
+ var scene,camera,renderer;
+ Init();
+ readSTLs('https://holodevuserresource.s3.amazonaws.com/apple2.stl'); 
+ animate();
+ function Init(){
+ 		scene = new THREE.Scene();
+ 		var VIEW_ANGLE = 2.64, ASPECT = windowWidth / windowHeight, NEAR = 0.1, FAR = 10000;
+ 		camera = new LeiaCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
+ 		camera.position.set(0,0,50);	
+ 		scene.add(camera);
+ 		renderer = new LeiaWebGLRenderer({
+         antialias:true, 
+ 		renderMode: _renderMode, 
+ 		camPanelVisible: _camPanelVisible, 
+ 		gyroPanelVisible: _gyroPanelVisible,
+ 		camFov: _camFov,    
+		devicePixelRatio: 1 
+      } );
+ 		renderer.Leia_setSize( windowWidth, windowHeight );
+ 		document.body.appendChild( renderer.domElement );
+ 		
+ 		var xl = new THREE.DirectionalLight( 0x555555 );
+ 			xl.position.set( 1, 0, 2 );
+ 			scene.add( xl );
+ 			var pl = new THREE.PointLight(0x111111);
+ 			pl.position.set(-20, 10, 20);
+ 			scene.add(pl);
+ 
+ 			var ambientLight = new THREE.AmbientLight(0x111111);	
+ 			scene.add(ambientLight);
+ 
+ 	time1 = Date.now() * 0.001; 
+ 	time0 = Date.now() * 0.001;
+ }
+ function readSTLs(filename) 
+ {
+ 	var xhr = new XMLHttpRequest();
+ 	xhr.onreadystatechange = function () {
+ 	if ( xhr.readyState == 4 ) {
+ 		if ( xhr.status == 200 || xhr.status == 0 ) {
+ 			var rep = xhr.response; // || xhr.mozResponseArrayBuffer;
+ 			mesh1 = parseStlBinary(rep);
+ 			mesh1.scale.set(sizeMesh1, sizeMesh1, sizeMesh1);
+ 			scene.add(mesh1);
+ 			mesh2 = parseStlBinary(rep);
+ 			mesh2.scale.set(sizeMesh2, sizeMesh2, sizeMesh2);
+ 			scene.add(mesh2);
+ 			newMeshReady = true;
+ 			}
+ 		}
+ 	};
+ 	xhr.onerror = function(e) {
+ 		console.log(e);
+ 	};
+ 	xhr.open( "GET", filename, true );
+ 	xhr.responseType = "arraybuffer";
+ 	xhr.send( null );
+ }
+ 
+ 
+ function animate() 
+ {
+ 	requestAnimationFrame( animate );
+ 	if (newMeshReady) {
+ 	time1 = Date.now() * 0.001; 
+ 	time_s = time1 - time0;
+ 		mesh1.position.set(-radius1*Math.cos(time_s)-1, 0, 1.0-radius1*Math.sin(time_s)+3); 
+ 		mesh2.position.set(radius2x*Math.cos(time_s)-1, -1, 1.0+radius2z*Math.sin(time_s)+3); 
+ 		mesh1.rotation.y = omega1*time_s;
+ 		mesh2.rotation.y = omega2*time_s;
+ 	}
+ 	//LEIA_render( scene, camera);
+ renderer.setClearColor(new THREE.Color().setRGB(1.0, 1.0, 1.0)); 
+	renderer.Leia_render(scene, camera);
+ }
+ //FileEnd
